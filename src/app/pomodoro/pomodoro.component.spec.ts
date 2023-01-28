9880eb518f72c80bebc624ff36b52680d37a8923
Charlotte Wilson <charlottewilson@charlottes-mbp.home>
2023-01-28 23:19:04 +0000

    basic render tests

--- src/app/pomodoro/pomodoro.component.spec.ts
+++ src/app/pomodoro/pomodoro.component.spec.ts
@@ -4,17 +4,9 @@ import { PomodoroComponent } from './pomodoro.component';
 
 describe('PomodoroComponent', () => {
   let component: PomodoroComponent;
-  let fixture: ComponentFixture<PomodoroComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PomodoroComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(PomodoroComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new PomodoroComponent();
   });
 
   it('should create', () => {
